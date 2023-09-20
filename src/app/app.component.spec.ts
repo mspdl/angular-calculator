6e44549d3a77f0fd398d59cdf0ae86c45d250963
Morgan <morgan.espindola@gmail.com>
2023-09-19 21:39:23 -0300

    Repository Project Migration

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { CalculatorModule } from './calculator';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [CalculatorModule],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
